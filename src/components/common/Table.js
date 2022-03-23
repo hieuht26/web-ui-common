b330b22dbfeb36b22eed264e704e04f3b06fd4fd
hieu.ht <hieuhoang2692@gmail.com>
2022-03-23 10:33:22 +0700

    Add library

---
+++ src/components/common/Table.js
@@ -0,0 +1,5 @@
+const Table = () => (
+  <div>Table</div>
+);
+
+export default Table;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 );
